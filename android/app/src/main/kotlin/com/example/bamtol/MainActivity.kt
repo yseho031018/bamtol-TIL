99aa73bda6f1d20b5cbac1909a8729240dbcc7a9
seho <sehoho1018@gmail.com>
2025-12-08 21:52:16 +0900

    Initial commit: 프로젝트 생성 및 기본 설정

---
+++ android/app/src/main/kotlin/com/example/bamtol/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bamtol
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:bamtol/src/app.dart';
+import 'package:bamtol/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: '당근마켓 클론코딩',
+      initialRoute: '/',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          color: Color(0xff212123),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        scaffoldBackgroundColor: const Color(0xff212123),
+      ),
+      getPages: [
+        GetPage(name: '/', page: () => const App()),
+      ],
+    );
+  }
+}
