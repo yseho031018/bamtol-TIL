3d2aaaf1008c96b91a980104bcc79f3284f8e3d0
seho <sehoho1018@gmail.com>
2025-12-08 23:30:53 +0900

    Week11: 최초 앱 화면 퍼블리싱 (shared_preferences 적용)

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:bamtol/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+late SharedPreferences prefs;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
